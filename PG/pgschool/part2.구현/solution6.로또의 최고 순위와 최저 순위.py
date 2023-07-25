3c7261a033baa7d9d7588d1bad457e435f42bdfa
kyeon06 <jkyeon06@gmail.com>
2023-07-26 01:27:34 +0900

    로또의 최고 순위와 최저 순위/다른풀이 추가

--- PG/pgschool/part2.구현/solution6.로또의 최고 순위와 최저 순위.py
+++ PG/pgschool/part2.구현/solution6.로또의 최고 순위와 최저 순위.py
@@ -24,3 +24,37 @@ def solution(lottos, win_nums):
         return [ 5-zero_cnt, 5]
     else:
         return [ 6-zero_cnt , 6]
+
+
+# 다른 풀이
+def solution1(lottos, win_nums):
+    cnt_corr = 0 # 당첨 가능한 최저 개수
+    cnt_zero = 0 # 알아볼 수 없는 번호
+    for i in range(len(lottos)):
+        if lottos[i] in win_nums:
+            cnt_corr += 1
+        if lottos[i] == 0:
+            cnt_zero += 1
+            
+    total = cnt_corr + cnt_zero # 당첨 가능한 최고 개수
+
+    rank = {6:1,5:2,4:3,3:4,2:5,1:6,0:6} # 순위와 당첨 내용 딕셔너리
+ 
+    answer = [rank[total],rank[cnt_corr]]
+    
+    return answer
+
+def solution2(lottos, win_nums):
+    answer = [0,0]
+    rank = [6,6,5,4,3,2,1]
+
+    cnt = 0
+    cntz = lottos.count(0) # 0개수 저장
+
+    for v in lottos:
+        if v in win_nums:
+            cnt += 1
+    
+    answer[0], answer[1] = rank[cnt+cntz], rank[cnt]
+
+    return answer
