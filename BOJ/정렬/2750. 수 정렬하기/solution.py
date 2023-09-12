70399db6541dcef1411eca23eab7962b2dc9f271
kyeon06 <jkyeon06@gmail.com>
2023-09-12 20:56:55 +0900

    move file

--- BOJ/정렬/2750. 수 정렬하기/solution.py
+++
@@ -1,7 +0,0 @@
-# 메모리 : 31256KB / 시간 : 80ms
-
-N = int(input())
-arr = [int(input()) for i in range(N)]
-
-for num in sorted(arr):
-    print(num)
--- PG/[PG]폰켓몬/solution.py
+++
@@ -1,9 +0,0 @@
-def solution(nums):
-    monster_num = len(set(nums))
-    get_monster = int(len(nums)/2)
-    
-    if get_monster < monster_num:
-        result = get_monster
-    else:
-        result = monster_num
-    return result
