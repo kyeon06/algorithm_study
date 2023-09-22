7cf9190dc9bf3f2154cd375c8b8c2dcdfa6152ee
kyeon06 <111492415+kyeon06@users.noreply.github.com>
2023-09-22 22:25:26 +0900

    [level 2] Title: 피보나치 수, Time: 81.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,15 @@
-def solution(n):
+# def solution(n):
 
-    num = [0,1]
-    for i in range(n):
-        num.append(num[i] + num[i+1])
+#     num = [0,1]
+#     for i in range(n):
+#         num.append(num[i] + num[i+1])
 
-    return num[n] % 1234567
+#     return num[n] % 1234567
+
+def solution(num):
+    
+    a, b = 0, 1
+    for i in range(num):
+        a, b = b, a+b
+        
+    return a % 1234567
