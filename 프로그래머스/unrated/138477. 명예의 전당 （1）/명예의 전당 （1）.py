f08d0c812f18bfa21ed111eb86708caab1e6c943
kyeon06 <111492415+kyeon06@users.noreply.github.com>
2023-09-22 21:57:28 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -3,15 +3,28 @@ def solution(k, score):
     best = []
     
     for s in score:
-        
-        if len(best) >= k:
-            if s < min(best):
-                answer.append(min(best))
-                continue
-            best = sorted(best, reverse=True)
-            best.pop()
-                
         best.append(s)
+        if len(best) > k:
+            best.remove(min(best))
+        
         answer.append(min(best))
-            
+    
     return answer
+
+# def solution(k, score):
+#     answer = []
+#     best = []
+    
+#     for s in score:
+        
+#         if len(best) >= k:
+#             if s < min(best):
+#                 answer.append(min(best))
+#                 continue
+#             best = sorted(best, reverse=True)
+#             best.pop()
+                
+#         best.append(s)
+#         answer.append(min(best))
+            
+#     return answer
