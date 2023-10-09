ad14029e306ba4528806fdd05a48bd2063a690b4
kyeon06 <111492415+kyeon06@users.noreply.github.com>
2023-10-09 21:11:17 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,12 +1,11 @@
 def solution(n):
     if n < 3:
         return n
+    a = 1
+    b = 2
     
-    chk = [0] * (n+1)
-    
-    chk[1] = 1
-    chk[2] = 2    
-    for i in range(3, n+1):
-        chk[i] = chk[i-1] + chk[i-2]
-    
-    return chk[n] % 1234567
+    for i in range(2, n):
+        a , b = b, a + b
+        
+    return b % 1234567
+
