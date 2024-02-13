65573c77d89a11fcf5bd950bcb3d2ba19d6ebc71
kyeon06 <111492415+kyeon06@users.noreply.github.com>
2024-02-13 22:17:01 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,12 +1,3 @@
 def solution(rsp):
-    answer = ''
-    
-    for i in rsp:
-        if i == "2":
-            answer += "0"
-        elif i == "0":
-            answer += "5"
-        else:
-            answer += "2"
-            
-    return answer
+    d = {"0":"5","2":"0","5":"2"} 
+    return ''.join([d[i] for i in rsp])
